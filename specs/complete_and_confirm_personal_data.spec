4420677241e72c5e4582a6796cc1f911de6121c8
John Boyes <john@agilepathway.co.uk>
2020-11-14 13:42:11 +0000

    Add onboarding concepts

---
+++ specs/complete_and_confirm_personal_data.spec
@@ -0,0 +1,7 @@
+# Complete and confirm personal data
+
+* Complete process up to being asked to complete and confirm personal data
+
+## Search
+* Go to Google homepage at "https://google.com"
+* Search for "Software testing"
--- step_impl/get_started.py
+++ step_impl/get_started.py
@@ -21,7 +21,6 @@ def init():
     options.add_argument('--ignore-certificate-errors')
     options.add_argument('--disable-dedv-shm-usage')
     options.add_argument("--no-sandbox")
-    options.add_argument("--headless")
 
     Driver.instance = webdriver.Chrome(chrome_options=options)
 
@@ -34,6 +33,38 @@ def go_to_get_started_page(query):
   textbox = Driver.instance.find_element_by_xpath("//input[@name='q']")
   textbox.send_keys(query)
   textbox.send_keys(Keys.RETURN)
+
+@step("Read overview of onboarding process")
+def read_overview_of_onboarding_process():
+    Driver.instance.get('https://example.com/read-overview-of-onboarding-process')
+
+@step("Select product package")
+def select_product_package():
+    Driver.instance.get('https://example.com/select-product-package')
+
+@step("Accept GDPR agreement")
+def accept_gdpr_agreement():
+    Driver.instance.get('https://example.com/accept-gdpr-agreement')
+
+@step("Confirm phone number")
+def confirm_phone_number():
+    Driver.instance.get('https://example.com/confirm-phone-number')
+
+@step("Allow access to camera for OCR & Liveness check")
+def allow_access_to_camera_for_ocr_and_liveness_check():
+    Driver.instance.get('https://example.com/allow-access-to-camera-for-ocr-and-liveness-check')
+
+@step("Confirm email address")
+def confirm_email_address():
+    Driver.instance.get('https://example.com/confirm-email-address')
+
+@step("Provide personal ID")
+def provide_personal_id():
+    Driver.instance.get('https://example.com/provide-carte-de-identitate')
+
+@step("Perform liveness checks")
+def perform_liveness_checks():
+    Driver.instance.get('https://example.com/perform-liveness-checks')
 
 @step("Go to Google homepage at <url>")
 def go_to_gauge_homepage_at(url):
